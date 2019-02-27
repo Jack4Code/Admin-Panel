0451cf69ed32c747e8264fe9579a00d406e14dfb
jack4code <jack.giannini@gmail.com>
2019-02-27 15:14:25 -0500

    redesigned architecture

--- js/index.js
+++ js/index.js
@@ -1,30 +1,30 @@
-const numOfServers = 2;
-const listOfServers = ['jackServer', 'juliaServer'];
-const UrlValues = 'http://localhost:61144/api/values';
+// const numOfServers = 2;
+// const listOfServers = ['jackServer', 'juliaServer'];
+// const UrlValues = 'http://localhost:61144/api/values';
 
-// $('.btn').click(function(){
-//     $.get(Url, function(data, status){
-//         console.log(`${data}`)
+// // $('.btn').click(function(){
+// //     $.get(Url, function(data, status){
+// //         console.log(`${data}`)
+// //     });
+// // })
+
+// function GetRequest(Url) {
+//     // $.ajax({
+//     //     url: Url,
+//     //     type: "GET",
+//     //     success: function (result) {
+//     //         console.log(result);
+//     //     },
+//     //     error: function (error) {
+//     //         console.log(`Error ${error}`);
+//     //     }
+//     // })
+//     $.getJSON(Url, function (result) {
+//         console.log(result)
 //     });
-// })
+// }
 
-function GetRequest(Url) {
-    // $.ajax({
-    //     url: Url,
-    //     type: "GET",
-    //     success: function (result) {
-    //         console.log(result);
-    //     },
-    //     error: function (error) {
-    //         console.log(`Error ${error}`);
-    //     }
-    // })
-    $.getJSON(Url, function (result) {
-        console.log(result)
-    });
-}
-
-GetRequest(UrlValues);
+// GetRequest(UrlValues);
 
 
 
